3e63ef6cac05252900900cc4791276f9287360ce
Aaron Rangel <aaron5025@hotmail.com>
2023-12-09 22:52:56 -0600

    Ross Improvements to stack datasets

--- stackDatasets.js
+++ stackDatasets.js
@@ -1,23 +1,13 @@
 
-
-
-
-
-
-
-
-
-
-
 var localization = {
     en: {
         title: "Stack Datasets",
         navigation: "Stack",
         out: "Enter the name of the stacked dataset",
-        in1: "Select the 1st dataset",
+        in1: "Select 2 or more datasets",
         in2: "Select the 2nd dataset",
         label1: "Stack Options",
-        label2: "Options below require both datasets to completely share the same variables",
+        label2: "All the options below require 2 datasets with exactly the same variable names",
         bindrows: "Bind Rows (stack datasets as is, may result in duplicate rows)",
         union: "Union (Stack datasets and remove exact duplicates)",
         intersect: "Intersect (Keep rows common to both datasets)",
@@ -28,9 +18,9 @@ var localization = {
             r_help: "help(bind_rows, package=dplyr)",
             body: `
             <b>Description</b></br>
-This dialog will help you stack 2 datasets on top of each other. You can select one of the following options.<br/>
-1. Bind Rows:  Stacks the 2 datasets exactly as they are. If a variable name is common to both datasets, values will fill in as you expect. If a dataset A contains a variable say var1 that is not present in the other dataset B, NA's will appear in variable var1 for all rows that correspond to dataset B.<br/>
-All options below require that both datasets share the same variables.<br/>
+This dialog will help you stack 2 or more datasets on top of each other. You can select one of the following options.<br/>
+1. Bind Rows:  Stacks the 2 or more datasets exactly as they are. If a variable name is common to both datasets, values will fill in as you expect. If a dataset A contains a variable say var1 that is not present in the other dataset B, NA's will appear in variable var1 for all rows that correspond to dataset B.<br/>
+All options below operate on ONLY 2 datasets and require that both datasets share the same variables.<br/>
 2. Union: stacks the datasets and removes duplicates<br/>
 3. Intersect: keeps rows common to both<br/>
 4. Difference: Keeps rows in 1st dataset, not in 2nd<br/>
@@ -39,7 +29,7 @@ NOTE: you can optionally track which dataset the original observation came from.
 <b>Package</b></br>
 dplyr</br>
 <b>Help</b></br>
-For detailed help click on the R icon on the top right hand side of this dialog overlay or run the following command help(bind_rows, package="dplyr") by creating a R code chunk by clicking + in the output window
+For detailed help click on the R icon on the top right hand side of this dialog overlay or run the following command help(bind_rows, package="dplyr") in the R syntax window
             `,
         }
     }
@@ -53,7 +43,7 @@ class stackDatasets extends baseModal {
             splitProcessing:false,
             RCode: `
 require(dplyr)
-{{selected.out | safe}} <- eval(parse(text = paste("{{selected.mergetype | safe}}" ,"(",{{selected.in1 | safe}}, ", ",{{selected.in2 | safe}}, ", .id = ", "{{selected.id | safe}}", ")", sep = "", collapse = "")))
+{{selected.out | safe}} <- {{selected.mergetype | safe}}( {{selected.in1 | safe}} {{selected.id | safe}})
 BSkyLoadRefreshDataframe( "{{selected.out | safe}}" )
 `,
         }
@@ -66,33 +56,25 @@ BSkyLoadRefreshDataframe( "{{selected.out | safe}}" )
                     placeholder: "",
                     extraction: "TextAsIs",
                     value: "",
+                    overwrite: "dataset",
                     type: "character",
                     required: true,
                 }),
             },
             in1: {
-                el: new dstVariable(config, {
-                    label: localization.en.in1,
-                    no: "in1",
-                    filter: "Dataset",
-                    extraction: "UseComma|Enclosed",
-                    required: true,
+                el: new dstVariableList(config,{
+                  label: localization.en.in1,
+                  no: "in1",
+                  filter: "Dataset",
+                  extraction: "UseComma",
+                  required: true,
                 })
-            },
-            in2: {
-                el: new dstVariable(config, {
-                    label: localization.en.in2,
-                    no: "in2",
-                    filter: "Dataset",
-                    extraction: "UseComma|Enclosed",
-                    required: true,
-                })
-            },
+              },
             label1: { el: new labelVar(config, { label: localization.en.label1, h: 5, style: "mt-1", }) },
             bindrows: {
                 el: new radioButton(config, { label: localization.en.bindrows, no: "mergetype", increment: "bindrows", value: "bind_rows", state: "checked", extraction: "ValueAsIs" })
             },
-            label2: { el: new labelVar(config, { label: localization.en.label2, h: 6, style: "mt-1", }) },
+            label2: { el: new labelVar(config, { label: localization.en.label2, h: 5, style: "mt-3", }) },
             union: {
                 el: new radioButton(config, { label: localization.en.union, no: "mergetype", increment: "union", value: "union", state: "", extraction: "ValueAsIs" })
             },
@@ -107,16 +89,18 @@ BSkyLoadRefreshDataframe( "{{selected.out | safe}}" )
                     no: 'id',
                     label: localization.en.id,
                     placeholder: "",
-                    extraction: "CreateArray",
+                    extraction: "ValueAsIs",
+                    wrapped: ", .id =\"%val%\"",
                     type: "character",
                     value: "",
+                    style: "mt-3",
                 }),
             },
         }
         const content = {
             head: [],
             left: [objects.dataset_var.el.content],
-            right: [objects.out.el.content, objects.in1.el.content, objects.in2.el.content, objects.label1.el.content, objects.bindrows.el.content, objects.label2.el.content, objects.union.el.content, objects.intersect.el.content, objects.setdiff.el.content, objects.id.el.content],
+            right: [objects.out.el.content, objects.in1.el.content,  objects.label1.el.content, objects.bindrows.el.content, objects.label2.el.content, objects.union.el.content, objects.intersect.el.content, objects.setdiff.el.content, objects.id.el.content],
             bottom: [],
             nav: {
                 name: localization.en.navigation,
